902d960e534bd2af48209e46ccc34575ba344910
JamesT94 <jamest9401@gmail.com>
2020-12-05 13:00:08 +0000

    initial setup
    ready for @Jack to write create/populate functions

---
+++ main-code.py
@@ -0,0 +1,26 @@
+"""
+Created on Dec 5th 2020
+
+@authors: James Taylor, Jack Taylor
+"""
+
+import math
+import numpy
+
+# Function to create a blank puzzle object @Jack
+
+
+
+
+# Function to populate a puzzle with initial numbers @Jack
+
+
+
+
+# Function to print a puzzle to the console in a way that is readable @Jack
+
+
+
+
+# Function to take a puzzle with initial numbers and complete it @James
+
